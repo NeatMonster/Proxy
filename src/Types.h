1bac3f030303583f87526d3daca787567bbab1c9
NeatMonster <neatmonster@hotmail.fr>
2014-12-02 19:52:48 +0100

    Ajout du ByteBuffer
    Nouveau ByteBuffer en partie réécrit. Il ne fait plus usage de l'operateur overloading. Les VarInts n'ont donc plus besoin de disposer de leur propre type.
    
    Signed-off-by: NeatMonster <neatmonster@hotmail.fr>

--- src/Types.h
+++ src/Types.h
@@ -5,15 +5,40 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
+// Taille maximale d'un paquet TCP.
+#define BUFFER_SIZE 65535
+
+// Définition de types de taille fixée.
+typedef std::int8_t byte_t;
 typedef std::uint8_t ubyte_t;
+typedef std::vector<byte_t> bytes_t;
+typedef std::vector<ubyte_t> ubytes_t;
+typedef std::int16_t short_t;
 typedef std::uint16_t ushort_t;
+typedef std::int32_t int_t;
 typedef std::uint32_t uint_t;
+typedef std::int64_t long_t;
 typedef std::uint64_t ulong_t;
+typedef float float_t;
+typedef double double_t;
 typedef std::string string_t;
 
 struct InvalidArgumentException : std::runtime_error {
     InvalidArgumentException(string_t s) : std::runtime_error(s) {}
 };
+
+size_t getSize(uint_t i) {
+    if (i < 128)
+        return 1;
+    else if (i < 16384)
+        return 2;
+    else if (i < 2097152)
+        return 3;
+    else if (i < 268435456)
+        return 4;
+    return 5;
+}
 
 #endif /* defined(__Proxy__Types__) */
